422af05b83913eb4a92e772f36291537090cc7a1
John Hurring Jr <jhurring@google.com>
2022-01-21 11:56:18 -0500

    First commit

---
+++ project-creation/shared-vpc-host/v1/iam.tf
@@ -0,0 +1,46 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  IAM
+ *****************************************/
+
+locals {
+  iam_roles = toset(local.iam_role_list)
+
+  iam_role_list = [
+    "roles/iam.serviceAccountAdmin",
+    "roles/monitoring.admin",
+    "roles/logging.admin",
+    "roles/compute.admin",
+    "roles/dns.admin",
+    "roles/container.admin",
+    "roles/artifactregistry.admin",
+    "roles/vpcaccess.admin",
+    "roles/secretmanager.admin",
+    "roles/cloudkms.admin",
+    "roles/servicedirectory.admin",
+    "roles/networkmanagement.admin",
+    "roles/networkconnectivity.hubAdmin",
+    "roles/resourcemanager.projectIamAdmin",
+  ]
+}
+
+resource "google_project_iam_member" "iam_member" {
+  for_each = local.iam_roles
+  project = google_project.project.project_id
+  member = "group:${var.project_admin_group}"
+  role   = each.value
+}
---
+++ project-creation/shared-vpc-host/v1/project-creation.tf
@@ -0,0 +1,37 @@
+/**
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/******************************************
+  Project Creation
+ *****************************************/
+
+resource "google_project" "project" {
+  name            = "${var.project_id_category}-${var.project_id_unique}-${random_string.random_string.result}"
+  project_id      = "${var.project_id_category}-${var.project_id_unique}-${random_string.random_string.result}"
+  billing_account = var.billing_account_id
+  folder_id       = var.folder_id
+}
+
+resource "random_string" "random_string" {
+  length = 4
+  lower = true
+  upper = false
+  special = false
+  number = true
+}
+
+resource "google_compute_shared_vpc_host_project" "project_to_shared_host" {
+  project = google_project.project.project_id
+}
