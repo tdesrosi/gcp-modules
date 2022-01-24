f3a7bd1fe291cd0787ec934bc7df9c8893c3ef7b
John Hurring Jr <jhurring@google.com>
2022-01-24 16:46:29 -0500

    Adding in cloud build private pool setup

--- cloud-build-triggers/private-pools/private-with-internet/v1/network.tf
+++ cloud-build-triggers/private-pools/private-with-internet/v1/network.tf
@@ -25,4 +25,24 @@ resource "google_compute_network" "network" {
   delete_default_routes_on_create = true
 }
 
+/******************************************
+  Subnet Network 1
+ *****************************************/
 
+resource "google_compute_subnetwork" "subnet" {
+  provider      = google-beta
+  project       = var.project_id
+  ip_cidr_range = var.cidr
+  name          = "${var.network_name}-${var.region}"
+  network       = google_compute_network.network.self_link
+  region        = var.region
+
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval  = var.vpc_flow_log_interval
+    flow_sampling         = var.vpc_flow_log_sampling
+    metadata              = "INCLUDE_ALL_METADATA"
+  }
+
+}
