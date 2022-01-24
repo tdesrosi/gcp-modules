c1cf0a114c384251cbb9a5efbc9e5aa76814306a
John Hurring Jr <jhurring@google.com>
2022-01-24 16:44:16 -0500

    Adding in cloud build private pool setup

--- cloud-build-triggers/private-pools/private-with-internet/v1/firewall-rules.tf
+++ cloud-build-triggers/private-pools/private-with-internet/v1/firewall-rules.tf
@@ -19,38 +19,6 @@ resource "google_compute_firewall" "firewall_rule_allow_199_egress" {
   }
 }
 
-
-
-resource "google_compute_firewall" "firewall_rule_deny_all_egress" {
-  project             = var.project_id
-  name                = "${var.network_name}-deny-all-egress-all"
-  network             = google_compute_network.network.self_link
-  direction           = "EGRESS"
-  priority            = 65000
-  destination_ranges  = ["0.0.0.0/0"]
-
-  log_config {
-    metadata = var.logging_metadata
-  }
-
-  deny {
-    protocol = "tcp"
-  }
-
-  deny {
-    protocol = "udp"
-  }
-
-  deny {
-    protocol = "icmp"
-  }
-
-  deny {
-    protocol = "ipip"
-  }
-
-}
-
 resource "google_compute_firewall" "firewall_rule_deny_all_ingress" {
   project         = var.project_id
   name            = "${var.network_name}-deny-all-ingress-all"
