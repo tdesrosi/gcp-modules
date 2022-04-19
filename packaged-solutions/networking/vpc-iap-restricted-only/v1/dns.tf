e99cc1c1c144179a7c6ecaa609e0ac3da79302a2
John Hurring Jr <jhurring@google.com>
2022-04-19 16:05:43 -0400

    Adding subnet

--- packaged-solutions/networking/vpc-iap-restricted-only/v1/dns.tf
+++ packaged-solutions/networking/vpc-iap-restricted-only/v1/dns.tf
@@ -85,3 +85,63 @@ resource "random_string" "random_string_for_gcrio_zone" {
   special = false
   number = true
 }
+
+/******************************************
+  Cloud DNS - Internal - gcrio
+ *****************************************/
+# run.app
+resource "google_dns_managed_zone" "restricted_run_app_zone" {
+  project     = var.project_id
+  name        = "restricted-run-app-zone-${random_string.random_string_for_runapp_zone.result}"
+  dns_name    = "run.app."
+  description = "Google Restricted API Access - Run App"
+  visibility  = "private"
+
+  private_visibility_config {
+    networks {
+      network_url =  google_compute_network.network.self_link
+    }
+  }
+}
+
+resource "google_dns_record_set" "restricted_run_app_a_record" {
+  project       = var.project_id
+  managed_zone  = google_dns_managed_zone.restricted_run_app_zone.name
+  name          = "run.app."
+  rrdatas       = ["199.36.153.4", "199.36.153.5", "199.36.153.6", "199.36.153.7"]
+  ttl           = 300
+  type          = "A"
+}
+
+resource "google_dns_record_set" "restricted_run_app_cname_record" {
+  project       = var.project_id
+  name          = "*.run.app."
+  managed_zone  = google_dns_managed_zone.restricted_run_app_zone.name
+  type          = "CNAME"
+  ttl           = 300
+  rrdatas       = ["run.app."]
+}
+
+resource "random_string" "random_string_for_api_zone" {
+  length = 4
+  lower = true
+  upper = false
+  special = false
+  number = true
+}
+
+resource "random_string" "random_string_for_gcrio_zone" {
+  length = 4
+  lower = true
+  upper = false
+  special = false
+  number = true
+}
+
+resource "random_string" "random_string_for_runapp_zone" {
+  length = 4
+  lower = true
+  upper = false
+  special = false
+  number = true
+}
